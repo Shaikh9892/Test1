f80e121a40128196c8d99038d903509325f3fb72
Shaikh9892 <irfamchuy@gmail.com>
2022-04-14 16:38:57 +0530

    First Commit

--- src/Test/DemoClass.java
+++ src/Test/DemoClass.java
@@ -4,7 +4,8 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Test OnE");
+		
 	}
 
 }
